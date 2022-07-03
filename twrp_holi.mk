e4041fd25ab352722d355429b25363630562ce6f
Spector <33796688+Spector0@users.noreply.github.com>
2022-07-03 11:12:47 +0530

    Update twrp_holi.mk

--- twrp_holi.mk
+++ twrp_holi.mk
@@ -27,7 +27,7 @@ $(call inherit-product, device/qualcomm/holi/device.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, vendor/twrp/config/gsm.mk)
+#(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := holi
